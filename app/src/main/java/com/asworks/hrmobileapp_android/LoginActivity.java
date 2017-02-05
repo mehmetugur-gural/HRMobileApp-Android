097c14fd6bab21f31f9b97e71798ccbc451ff027
Mehmet Ugur Gural <delphoiq@gmail.com>
2017-02-06 01:46:58 +0300

    login screen implementation

--- app/src/main/java/com/asworks/hrmobileapp_android/LoginActivity.java
+++ app/src/main/java/com/asworks/hrmobileapp_android/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.asworks.hrmobileapp_android;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //setTitle(R.string.login_title);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.title_layout);
+        SetCustomTitle(getString(R.string.login_title));
+    }
+
+    void SetCustomTitle(String title)
+    {
+        TextView textViewTitle = (TextView) findViewById(R.id.title_text);
+        textViewTitle.setText(title);
     }
 }
