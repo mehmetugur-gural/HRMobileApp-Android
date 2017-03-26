6cd207132b661c0f54ae5b573c4c4da6bbf5c062
Mehmet Ugur Gural <delphoiq@gmail.com>
2017-03-26 23:30:34 +0300

    register page implementation

--- app/src/main/java/com/asworks/hrmobileapp_android/RegisterActivity.java
+++ app/src/main/java/com/asworks/hrmobileapp_android/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,9 +28,51 @@ public class RegisterActivity extends AppCompatActivity {
     private View.OnClickListener btnRegisterClick = new View.OnClickListener() {
         public void onClick(View v) {
 
-        Toast.makeText(getApplicationContext(), "Üyeliğiniz Yöneticilerimiz Tarafından Onaylandıktan Sonra Aktif Olacaktır.", Toast.LENGTH_SHORT).show();
+            if (validateRegisterForm())
+            {
+                Toast.makeText(getApplicationContext(), "Üyeliğiniz Yöneticilerimiz Tarafından Onaylandıktan Sonra Aktif Olacaktır.", Toast.LENGTH_SHORT).show();
+            }
         }
     };
+
+    public boolean validateRegisterForm()
+    {
+        boolean result = true;
+
+        EditText firstName = (EditText)findViewById(R.id.txtRegisterName);
+        EditText lastName = (EditText)findViewById(R.id.txtRegisterSurname);
+        EditText mailAddress = (EditText)findViewById(R.id.txtRegisterMail);
+        EditText password = (EditText)findViewById(R.id.txtRegisterPassword);
+        EditText password2 = (EditText)findViewById(R.id.txtRegisterPassword2);
+
+        if(firstName.getText().toString().length() == 0 )
+        {firstName.setError( "Lütfen Adınızı Giriniz!" );
+         result = false;}
+
+        if(lastName.getText().toString().length() == 0 )
+        {lastName.setError( "Lütfen Soyadınızı Giriniz!" );
+            result = false;}
+
+        if(mailAddress.getText().toString().length() == 0 )
+        {mailAddress.setError( "Lütfen Mail Adresinizi Giriniz!" );
+            result = false;}
+
+        if(password.getText().toString().length() == 0 )
+        {password.setError( "Lütfen Şifrenizi Giriniz!" );
+            result = false;}
+
+        if(password2.getText().toString().length() == 0 )
+        {password2.setError( "Lütfen Şifrenizi Tekrar Giriniz!" );
+            result = false;}
+
+        if (!password.getText().toString().equals(password2.getText().toString()))
+        {
+            password2.setError( "Lütfen Şifrenizi Aynı Olacak Şekilde Giriniz!" );
+            result = false;
+        }
+
+        return  result;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
